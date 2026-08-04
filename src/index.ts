17583738514d164ccdd6864d73c66e0bb11371d2
Nus Kus <nkus7918@gmail.com>
2026-08-04 14:00:33 +0300

    fix: increase express body size limit

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,14 @@ app.use(
 );
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit: "50mb",
+}));
+
+app.use(express.urlencoded({
+  extended: true,
+  limit: "50mb",
+}));
 
 app.get("/health", (_req, res) => {
   res.json({
