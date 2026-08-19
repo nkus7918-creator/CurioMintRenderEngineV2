efd774148c375d14d3c660efe5bb20d0b5c88be4
Nus Kus <nkus7918@gmail.com>
2026-08-19 13:43:00 +0300

    Use separate display and subtitle fonts

--- src/templates/curiomint-scenes/fonts.ts
+++ src/templates/curiomint-scenes/fonts.ts
@@ -1,8 +1,15 @@
-import { loadFont } from "@remotion/google-fonts/Anton";
+import { loadFont as loadAnton } from "@remotion/google-fonts/Anton";
+import { loadFont as loadMontserrat } from "@remotion/google-fonts/Montserrat";
 
-const anton = loadFont("normal", {
+const anton = loadAnton("normal", {
   weights: ["400"],
   subsets: ["latin", "latin-ext"],
 });
 
+const montserrat = loadMontserrat("normal", {
+  weights: ["800"],
+  subsets: ["latin", "latin-ext"],
+});
+
 export const SHORTS_FONT_FAMILY = anton.fontFamily;
+export const SUBTITLE_FONT_FAMILY = montserrat.fontFamily;
